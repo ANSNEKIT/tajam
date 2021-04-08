a98aa5a228dd99730331b1dbe872645648a17d50
ANSNEKIT <ansnekit@gmail.com>
2021-04-08 12:17:18 +0300

    Исправлены ошибки
    Ошибки в верстке иконок соц. сетей, ошибки путей.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,28 @@
-"use strict";var $modal=document.getElementById("modal-video"),$modalClose=document.getElementById("modal-close"),$modalShadow=document.querySelector(".modal-backdrop"),$aboutBtn=document.getElementById("about-btn"),$commentsBlock=document.querySelector("#people .people__list");$aboutBtn.addEventListener("click",function(){$modal.classList.add("show"),$modalShadow.classList.add("show")}),$modalClose.addEventListener("click",function(){$modal.classList.remove("show"),$modalShadow.classList.remove("show")}),$commentsBlock.addEventListener("click",function(e){var t=document.querySelector("#people li.people__item.active"),o=e.target;"IMG"===e.target.tagName&&(a=o.getAttribute("data-id"));var a=document.querySelector('#people li.people__item[data-id="'.concat(a,'"]'));t.classList.remove("active"),a.classList.add("active")});
+'use strict';
+
+var $modal = document.getElementById('modal-video');
+var $modalClose = document.getElementById('modal-close');
+var $modalShadow = document.querySelector('.modal-backdrop');
+var $aboutBtn = document.getElementById('about-btn');
+var $commentsBlock = document.querySelector('#people .people__list');
+$aboutBtn.addEventListener('click', function () {
+  $modal.classList.add('show');
+  $modalShadow.classList.add('show');
+});
+$modalClose.addEventListener('click', function () {
+  $modal.classList.remove('show');
+  $modalShadow.classList.remove('show');
+});
+$commentsBlock.addEventListener('click', function (evt) {
+  var $liCurrent = document.querySelector("#people li.people__item.active");
+  var $elem = evt.target;
+  var id;
+
+  if (evt.target.tagName === "IMG") {
+    id = $elem.getAttribute('data-id');
+  }
+
+  var $liClicked = document.querySelector("#people li.people__item[data-id=\"".concat(id, "\"]"));
+  $liCurrent.classList.remove('active');
+  $liClicked.classList.add('active');
+});
